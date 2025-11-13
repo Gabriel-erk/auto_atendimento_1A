8ee1f8fc36d88d0c7fc00e1d44c02c18ffec88aa
Emanuel Souza <111304925+ManuSouzaLima@users.noreply.github.com>
2025-11-13 04:05:41 -0300

    Conexão com o banco de dados/ post no banco de dados

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hospital"; // ou coloca aqui o nome do banco que voce criar no workbanch
+
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+if ($conn->connect_error){
+    die("conexao falhou: $conn->connect_error <br><br>");
+}
+else{
+    echo "conexao feita <br><br>";
+}
+
+
+?>
