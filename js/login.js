1d410a17313ddfa1d0b912b5659d54474b57a7eb
Emanuel Souza <111304925+ManuSouzaLima@users.noreply.github.com>
2025-11-15 17:38:09 -0300

    Login Concluido - bate no BD e confere se existe
    Added user feedback messages for login failure.

--- js/login.js
+++ js/login.js
@@ -19,13 +19,25 @@ form.addEventListener("submit",(evento)=>{
         .then(aviso => {
 
             if(aviso.success == false){
-                console.log("Não existe o usuario");
+                var msg = document.createElement("div")
+                msg.classList.add("p-3");
+                msg.classList.add("w-100");
+                msg.classList.add("mt-2");
+                msg.classList.add("text-danger-emphasis");
+                msg.classList.add("bg-danger-subtle");
+                msg.classList.add("border");
+                msg.classList.add("border-danger-subtle");
+                msg.classList.add("rounded-3");
+                msg.textContent = "Usuario não existe"
+                var div_mensagens = document.querySelector("#invalido");
+                div_mensagens.appendChild(msg);
             }
             else{
-                console.log("existe usuario");
+                window.location = "home.html";
             }
         })
 })
+
 
 
 
