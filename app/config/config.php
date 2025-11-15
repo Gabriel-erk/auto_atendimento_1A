1095aad7cbb1f879edf5c433ac3d6e951e038930
Emanuel Souza <111304925+ManuSouzaLima@users.noreply.github.com>
2025-11-15 03:04:45 -0300

    Refactor database connection code in config.php

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "";       //PUXAR COM O .ENV
 $dbname = "hospital"; // ou coloca aqui o nome do banco que voce criar no workbanch
 
 $conn = new mysqli($servername,$username,$password,$dbname);
@@ -10,9 +10,7 @@ $conn = new mysqli($servername,$username,$password,$dbname);
 if ($conn->connect_error){
     die("conexao falhou: $conn->connect_error <br><br>");
 }
-else{
-    echo "conexao feita <br><br>";
-}
 
 
 ?>
+
